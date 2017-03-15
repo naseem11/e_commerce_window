ae51b901dc53eddfc9212d8fd7772bce4a9991dd
Naseem <miannaseem11@gmail.com>
2017-03-15 14:47:21 +0000

    product model added

--- products/models.py
+++ products/models.py
@@ -3,3 +3,9 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Products(models.Model):
+    name=models.CharField(max_length=250,default='')
+    description=models.TextField()
+    price=models.DecimalField( max_digits=6 ,decimal_places=2,)
+    def __str__(self):
+        return self.name
